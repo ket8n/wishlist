886645eb14977275d9f69732da1cbd7f4e8535ac
ket8n <ketanchopade.kc@gmail.com>
2025-04-19 15:45:58 +0530

    Add: github credentials for oauth service

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -8,6 +8,9 @@ export default {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    GitHub({}),
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
   ],
 } satisfies NextAuthConfig;
