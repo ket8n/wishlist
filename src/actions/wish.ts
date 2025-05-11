3fc6e8846ff31aec300061d89304ea1e1666b4f8
ket8n <ketanchopade.kc@gmail.com>
2025-05-11 22:36:55 +0530

    Add: crud operations for wish

---
+++ src/actions/wish.ts
@@ -0,0 +1,59 @@
+"use server";
+
+import { prisma } from "../../prisma/prisma";
+import { WishStatus, PriorityLevel } from "@/generated/prisma";
+
+export async function createWish(data: {
+  name: string;
+  description: string;
+  links?: string[];
+  status?: WishStatus;
+  priority?: PriorityLevel;
+  isPublic?: boolean;
+  userId: string;
+}) {
+  return await prisma.wish.create({
+    data: {
+      name: data.name,
+      description: data.description,
+      links: data.links || [],
+      status: data.status || "PENDING",
+      priority: data.priority || "MEDIUM",
+      isPublic: data.isPublic || false,
+      userId: data.userId,
+    },
+  });
+}
+
+export async function getUsersWishes(userId: string) {
+  return await prisma.wish.findMany({
+    where: { userId },
+    orderBy: { createdAt: "desc" },
+  });
+}
+
+export async function getWishById(id: string) {
+  return await prisma.wish.findUnique({ where: { id } });
+}
+
+export async function updateWish(
+  id: string,
+  data: Partial<{
+    name: string;
+    description: string;
+    links?: string[];
+    status?: WishStatus;
+    priority?: PriorityLevel;
+    isPublic?: boolean;
+    fulfilledOn: Date;
+  }>
+) {
+  return await prisma.wish.update({
+    where: { id },
+    data: { ...data, updatedAt: new Date() },
+  });
+}
+
+export async function deleteWish(id: string) {
+  return await prisma.wish.delete({ where: { id } });
+}
