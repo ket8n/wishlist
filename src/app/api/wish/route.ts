4958afda94f9b8ad9fc93e9a69761ad1f178bfd1
ket8n <ketanchopade.kc@gmail.com>
2025-05-12 20:06:06 +0530

    Add: error handling the route apis

--- src/app/api/wish/route.ts
+++ src/app/api/wish/route.ts
@@ -4,33 +4,61 @@ import { CreateWishSchema } from "@/lib/wishValidations";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const session = await auth();
-  console.log(session);
-  if (!session?.user?.id) {
-    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  try {
+    const session = await auth();
+    console.log(session);
+    if (!session?.user?.id) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const body = await req.json();
+
+    const parsed = CreateWishSchema.safeParse(body);
+    if (!parsed.success) {
+      return NextResponse.json({ error: parsed.error.errors }, { status: 400 });
+    }
+
+    const newWish = await createWish({
+      ...parsed.data,
+      userId: session.user.id,
+    });
+
+    return NextResponse.json(
+      {
+        message: "Wish created successfully.",
+        wish: newWish,
+      },
+      { status: 201 }
+    );
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return NextResponse.json({ message: err.message }, { status: 500 });
+    }
+
+    return NextResponse.json(
+      { message: "Unknown error occured!" },
+      { status: 500 }
+    );
   }
-
-  const body = await req.json();
-
-  const parsed = CreateWishSchema.safeParse(body);
-  if (!parsed.success) {
-    return NextResponse.json({ error: parsed.error.errors }, { status: 400 });
-  }
-
-  const newWish = await createWish({ ...parsed.data, userId: session.user.id });
-
-  return NextResponse.json({
-    message: "Wish created successfully.",
-    wish: newWish,
-  });
 }
 
 export async function GET() {
-  const session = await auth();
-  if (!session?.user?.id) {
-    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-  }
+  try {
+    const session = await auth();
+    if (!session?.user?.id) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
 
-  const wishes = await getUsersWishes(session.user.id);
-  return NextResponse.json(wishes);
+    const wishes = await getUsersWishes(session.user.id);
+    return NextResponse.json(wishes);
+  } catch (err) {
+    if (err instanceof Error) {
+      return NextResponse.json({ message: err.message }, { status: 500 });
+    }
+
+    return NextResponse.json(
+      { message: "Unknown error occured!" },
+      { status: 500 }
+    );
+  }
 }
